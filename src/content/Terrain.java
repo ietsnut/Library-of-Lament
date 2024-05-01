6e043fa40fb180515246c7f02a51b47b6b65c123
ietsnut <marijnbrussel@hotmail.com>
2024-05-01 14:52:16 +0200

    ready to add sliding and slope climbing

--- src/content/Terrain.java
+++ src/content/Terrain.java
@@ -16,63 +16,27 @@ public class Terrain extends Model {
         enqueue();
     }
 
-    public float move(Vector3f newPlayerWorldPosition) {
+    public float move(Vector3f origin, Vector3f movement) {
+        Vector3f pos = new Vector3f(origin).add(movement);
         for (int i = 0; i < vertices.length; i += 9) {
             if (normals[i + 1] < 0.7071) {
                 continue;
             }
-            Vector3f v1 = new Vector3f(vertices[i], vertices[i + 1], vertices[i + 2]);
-            Vector3f v2 = new Vector3f(vertices[i + 3], vertices[i + 4], vertices[i + 5]);
-            Vector3f v3 = new Vector3f(vertices[i + 6], vertices[i + 7], vertices[i + 8]);
-            if (pointInXZTriangle(newPlayerWorldPosition, v1, v2, v3)) {
-                System.out.println("triangle example: " + v1 + ", " + v2 + ", " + v3);
-                return barryCentric(v1, v2, v3, new Vector2f(newPlayerWorldPosition.x, newPlayerWorldPosition.z));
+            float[] barry = barry(vertices[i], vertices[i + 2], vertices[i + 3], vertices[i + 5], vertices[i + 6], vertices[i + 8], pos.x, pos.z);
+            if ((0 <= barry[0] && barry[0] <= 1) && (0 <= barry[1] && barry[1] <= 1) && (0 <= barry[2] && barry[2] <= 1)) {
+                return barry[0] * vertices[i + 1] + barry[1] * vertices[i + 4] + barry[2] * vertices[i + 7];
             }
         }
         return -1;
     }
 
-    private boolean pointInXZTriangle(Vector3f pt, Vector3f v1, Vector3f v2, Vector3f v3) {
-        float denominator = (v2.z - v3.z) * (v1.x - v3.x) + (v3.x - v2.x) * (v1.z - v3.z);
-        float a = ((v2.z - v3.z) * (pt.x - v3.x) + (v3.x - v2.x) * (pt.z - v3.z)) / denominator;
-        float b = ((v3.z - v1.z) * (pt.x - v3.x) + (v1.x - v3.x) * (pt.z - v3.z)) / denominator;
-        float c = 1 - a - b;
-        return (0 <= a && a <= 1) && (0 <= b && b <= 1) && (0 <= c && c <= 1);
-    }
-
-    private float barryCentric(Vector3f p1, Vector3f p2, Vector3f p3, Vector2f pos) {
-        float det = (p2.z - p3.z) * (p1.x - p3.x) + (p3.x - p2.x) * (p1.z - p3.z);
-        float l1 = ((p2.z - p3.z) * (pos.x - p3.x) + (p3.x - p2.x) * (pos.y - p3.z)) / det;
-        float l2 = ((p3.z - p1.z) * (pos.x - p3.x) + (p1.x - p3.x) * (pos.y - p3.z)) / det;
+    private float[] barry(byte x1, byte z1, byte x2, byte z2, byte x3, byte z3, float x, float y) {
+        float det = (z2 - z3) * (x1 - x3) + (x3 - x2) * (z1 - z3);
+        float l1 = ((z2 - z3) * (x - x3) + (x3 - x2) * (y - z3)) / det;
+        float l2 = ((z3 - z1) * (x - x3) + (x1 - x3) * (y - z3)) / det;
         float l3 = 1.0f - l1 - l2;
-        return l1 * p1.y + l2 * p2.y + l3 * p3.y;
+        return new float[]{ l1, l2, l3 };
     }
-
-    /*
-    public float move(Vector3f newPlayerWorldPosition) {
-
-        // the vertices are already in world space, so we can use them directly
-        // the vertices are bytes because they are in the range -128 to 127, you can use them directly as floats
-        byte[] vertices = this.vertices;
-        float[] normals = this.normals;
-
-        //TODO: go through the vertexes and their normals and find the triangle the player is on.
-        // the playerworldposition y is always at its feet (0)
-        // the vertices are already in world space, so we can use them directly
-        // Look if the position is on a horizontal plane (normal up) or slope of max 45 degrees, otherwise return -1
-        // use the normals array to look up the corresponding normal for the vertex
-        // return the height of the terrain at the player position
-        // you can use JOML
-        // also make sure that vertices above the player are not considered (since there could be a ceiling above the player)
-
-        System.out.println("position example: " + newPlayerWorldPosition);
-        System.out.println("triangle example: " + vertices[4] + ", " + vertices[2] + ", " + vertices[3]);
-        System.out.println("normal example: " + normals[1] + ", " + normals[2] + ", " + normals[3]);
-
-        return -1;
-
-
-    }*/
 
     @Override
     public void load() {
--- src/object/Camera.java
+++ src/object/Camera.java
@@ -19,7 +19,7 @@ public class Camera extends Thread {
         - add dynamic FOV
     */
 
-    public static Transformation transformation     = new Transformation().translate(1, 1, -1.5f);
+    public static Transformation transformation     = new Transformation().translate(1, 1, 1.5f);
     public static Matrix4f view                     = new Matrix4f();
 
     public static final float SPEED    = 0.05f;
@@ -32,25 +32,25 @@ public class Camera extends Thread {
             Vector3f forward = new Vector3f(transformation.forward().x, 0, transformation.forward().z).normalize();
 
             Vector3f origin = new Vector3f(transformation.position);
-            Vector3f direction = new Vector3f();
+            Vector3f movement = new Vector3f();
 
             if (Control.isKeyDown(GLFW_KEY_W) || Control.isKeyDown(GLFW_KEY_UP)) {
-                direction.add(forward.x * -SPEED, 0, forward.z * -SPEED);
+                movement.add(forward.x * -SPEED, 0, forward.z * -SPEED);
             }
             if (Control.isKeyDown(GLFW_KEY_S) || Control.isKeyDown(GLFW_KEY_DOWN)) {
-                direction.add(forward.x * SPEED, 0, forward.z * SPEED);
+                movement.add(forward.x * SPEED, 0, forward.z * SPEED);
             }
             if (Control.isKeyDown(GLFW_KEY_D) || Control.isKeyDown(GLFW_KEY_RIGHT)) {
-                direction.add(forward.z * SPEED, 0, forward.x * -SPEED);
+                movement.add(forward.z * SPEED, 0, forward.x * -SPEED);
             }
             if (Control.isKeyDown(GLFW_KEY_A) || Control.isKeyDown(GLFW_KEY_LEFT)) {
-                direction.add(forward.z * -SPEED, 0, forward.x * SPEED);
+                movement.add(forward.z * -SPEED, 0, forward.x * SPEED);
             }
 
-            Vector3f position = new Vector3f(origin).add(direction);
+            Vector3f position = new Vector3f(origin).add(movement);
 
             if (Game.scene.terrain() != null) {
-                float height = Game.scene.terrain().move(new Vector3f(position));
+                float height = Game.scene.terrain().move(new Vector3f(origin), new Vector3f(movement));
                 System.out.println(height);
                 if (height >= 0) {
                     transformation.position(position);
